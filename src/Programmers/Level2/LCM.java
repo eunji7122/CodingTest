2ef13c8d68da2d43d6aef47541baacf37ca6ba30
eunji <lej7122@gmail.com>
2022-04-12 21:48:55 +0900

    Complete LCM

--- src/Programmers/Level2/LCM.java
+++ src/Programmers/Level2/LCM.java
@@ -2,6 +2,26 @@ package Programmers.Level2;
 
 public class LCM {
     public static void main(String[] args) {
+        solution(new int[] {2, 3, 4});
+    }
 
+    public static int solution(int[] arr) {
+        if (arr.length == 1)
+            return arr[0];
+
+        int lcm = 1;
+        for (int k : arr) {
+            int range = Math.min(k, lcm);
+
+            int tmp = 0;
+            for (int j = 1; j <= range; j++) {
+                if (k % j == 0 && lcm % j == 0) {
+                    tmp = (k / j) * (lcm / j) * j;
+                }
+            }
+            lcm = tmp;
+        }
+
+        return lcm;
     }
 }
